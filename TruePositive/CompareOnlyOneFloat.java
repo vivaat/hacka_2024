cbc68981355aedd27f1d9303dda9686b45eeb2ed
UseCpp <82133938+UseCpp@users.noreply.github.com>
2024-06-30 18:02:22 +0300

    Delete TruePositive/CompareOnlyOneFloat.java

--- TruePositive/CompareOnlyOneFloat.java
+++
@@ -1,7 +0,0 @@
-public class CompareOnlyOneFloat {
-    Float eFloat;
-
-    public boolean depthCompare(CompareOnlyOneFloat comparedObject) {
-        return eFloat == comparedObject.eFloat || eFloat.equals(comparedObject.eFloat);
-    }
-}
