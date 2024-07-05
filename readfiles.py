c306e3379d0a95e53a1358785667e9040477ed0b
user <user>
2024-07-05 14:42:10 +0300

    ADD: get_example_files.

--- readfiles.py
+++ readfiles.py
@@ -1,6 +1,7 @@
 from os          import listdir
 from os.path     import isfile, join
 from dataclasses import dataclass
+from tempfile    import NamedTemporaryFile
 
 
 @dataclass
@@ -25,3 +26,70 @@ def readdir(dirname):
 def fileFactory(filename, directory, label):
     fullpath = join(directory, filename)
     return File(filename, fullpath, readfile(fullpath), label)
+
+ 
+
+import os
+from pathlib import Path
+
+
+def get_strings_from_targets_json() -> list[str]:
+    """ Читает .json файл с частями кода для обработки нейросетью
+    :return: Список строк для определения качества срабатывания
+    """
+
+    settings = {
+        "locations": [
+            [
+                {
+                    "start": 52,
+                    "end": 52
+                },
+                {
+                    "start": 114,
+                    "end": 126
+                }
+            ],
+            [
+                {
+                    "start": 74,
+                    "end": 74
+                },
+                {
+                    "start": 92,
+                    "end": 104
+                }
+            ]
+        ],
+        "parent": {"start": 36, "end": 136}
+    }
+
+    dir_path = Path(os.path.dirname(os.path.realpath(__file__))).absolute()
+    p = os.path.join(dir_path, "KeycloakExamples", "Main.java")
+    settings['file'] = str(p)
+
+    with open(settings["file"], 'r', encoding='utf-8') as code:
+        content = code.readlines()
+        output = []
+        for location in settings['locations']:
+            el = []
+            for target in location:
+                if target["start"] != target["end"]:
+                    el.append(
+                        '\n'.join(content[target["start"]:target["end"] + 1])
+                    )
+                else:
+                    el.append(content[target["start"] - 1])
+            output.append(el)
+
+        return output
+    
+def get_example_files():
+    filenames = []
+    for el in get_strings_from_targets_json():
+        tmp = NamedTemporaryFile(mode='w', delete=False)     
+        for line in el:
+            tmp.write(line)
+        filenames.append(tmp.name)
+        tmp.close()
+    return filenames
--- tokenizer_usage.py
+++ tokenizer_usage.py
@@ -1,6 +1,6 @@
 from argparse  import ArgumentParser
 from tokenizer import PreParser, Parser
-
+from readfiles import get_example_files
 
 parser = ArgumentParser(
                     prog='test_tokenizer.py',
@@ -14,9 +14,14 @@ args      = parser.parse_args()
 file_path = args.file
 
 
-with PreParser(file_path) as preparser:
-    preparser.parse()
-    with Parser( preparser.get_result() ) as parser:
-        parser.parse()
-        for value in parser.get_result():
-            print(value)
+
+
+for file_path in get_example_files():
+    print()
+    print("FILE NAME IS ", file_path)
+    with PreParser(file_path) as preparser:
+        preparser.parse()
+        with Parser( preparser.get_result() ) as parser:
+            parser.parse()
+            for value in parser.get_result():
+                print(value)
